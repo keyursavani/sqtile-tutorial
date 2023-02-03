dcdff03732c04e2f5e9f00e198640a5bbf384927
keyursavani <savanikeyur1011@gmail.com>
2023-02-03 09:02:36 +0530

    final commit

--- lib/db_handler.dart
+++ lib/db_handler.dart
@@ -21,19 +21,26 @@ class DBHelper {
 
     io.Directory documentDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentDirectory.path, 'notes.db');
-    var db = await openDatabase(path,version: 1 , onCreate: _onCreate);
+    var db = await openDatabase(path , version: 1 ,onCreate: _onCreate);
     return db;
   }
 
   _onCreate (Database db, int version)async{
-    await db.execute("CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT , titel TEXT NOT NULL , age INTEGER NOT NULL , description TEXT NOT NULL , email TEXT)",
+    await db.execute(
+      "CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT , title TEXT NOT NULL , age INTEGER NOT NULL , description TEXT NOT NULL , email TEXT)",
 
     );
   }
 
   Future<NotesModel> insert(NotesModel notesModel) async{
     var dbClient = await db;
-    await dbClient!.insert('notes',notesModel.toMap());
+    await dbClient!.insert('notes', notesModel.toMap());
     return notesModel;
+  }
+
+  Future<List<NotesModel>> getNotesList () async{
+    var dbClient = await db;
+    final List<Map<String , Object?>> queryResult = await dbClient!.query('notes');
+    return queryResult.map((e) => NotesModel.fromeMap(e)).toList();
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,35 +7,43 @@ class HomeScreen extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return new _HomeScreenState();
+    return  _HomeScreenState();
   }
 }
 
 class _HomeScreenState extends State<HomeScreen>{
+
   DBHelper? dbHelper;
+
+  @override
+  void initState(){
+    // TODO: implement initState
+    super.initState();
+    dbHelper = DBHelper();
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-   return new Scaffold(
-     appBar: new AppBar(
-       title: new Text("Notes SQL"),
+   return Scaffold(
+     appBar: AppBar(
+       title: Text("Notes SQL"),
        centerTitle: true,
      ),
-     body: new Column(
+     body: Column(
        children: [
        ],
      ),
-     floatingActionButton: new FloatingActionButton(
+     floatingActionButton: FloatingActionButton(
        onPressed : (){
          dbHelper!.insert(
-             new NotesModel(
+                NotesModel(
                title: 'First Note',
                age : 22,
                description: 'This is My first sql app',
                email : 'savanikeyur1011@gmail.com'
              )
          ).then((value){
-           print("data added");
+           print('data added');
            }).onError((error, stackTrace){
              print(error.toString());
          });
